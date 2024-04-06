98eab5d3ae89d55467595e19aab8b634253b2496
Khang2010George <trandaikhang9510@gmail.com>
2024-04-06 22:19:06 +0700

    []

---
+++ custom_widget/CustomListItem.py
@@ -0,0 +1,31 @@
+from PyQt6 import QtWidgets
+
+class CustomListItemWidget(QtWidgets.QWidget):
+    def __init__(self, id, title, content):
+        self.id = id
+        self.title = title
+        self.content = content
+        super().__init__()
+        self.setAutoFillBackground(True)
+        
+        closeButton = QtWidgets.QPushButton('X')
+        closeButton.setFixedSize(20, 20)
+        # closeButton.clicked.connect(self.onCloseClicked)
+
+        titleLabel = QtWidgets.QLabel(title)
+        contentLabel = QtWidgets.QLabel(' '.join(content.split()[:5]) + '...')
+
+        titleFont = titleLabel.font()
+        titleFont.setPointSize(titleFont.pointSize() + 4)
+        titleFont.setBold(True)
+        titleLabel.setFont(titleFont)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(titleLabel)
+        layout.addWidget(contentLabel)
+        self.setLayout(layout)
+    
+    def update(self):
+        self.layout().itemAt(0).widget().setText(self.title)
+        self.layout().itemAt(1).widget().setText(' '.join(self.content.split()[:5]) + '...')
+        self.layout().itemAt(2).widget().setText(self.updated_at)
